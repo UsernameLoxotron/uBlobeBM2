acf44dda0050c4a5a289bd8960599ffe2cd131a0
UsernameLoxotron <68998003+UsernameLoxotron@users.noreply.github.com>
2024-12-11 16:15:12 -0500

    Update blob.js

--- blob.js
+++ blob.js
@@ -2,7 +2,7 @@
 document.addEventListener('DOMContentLoaded', function() {
     if (window.location.href !== 'https://usernameloxotron.github.io/uBlobeBM2/main.html') {
         var script = document.createElement('script');
-        script.src = 'main.js'; 
+        script.src = 'https://usernameloxotron.github.io/uBlobeBM2/main.js'; 
         document.body.appendChild(script);
     }
 });
