f9499baeaf4cccad7dcf06b927d87fbcca40c50f
UsernameLoxotron <68998003+UsernameLoxotron@users.noreply.github.com>
2024-12-10 20:19:11 -0500

    Update main.js

--- main.js
+++ main.js
@@ -50,7 +50,7 @@ document.addEventListener("keydown", function(blob) {
         bar.style.cssText = `
             width: 100%;
             height: 34px;
-            background-color: #4CAF50;
+            background-color: #164a18;
             position: relative;
             border-top-left-radius: 8px;
             border-top-right-radius: 8px;
